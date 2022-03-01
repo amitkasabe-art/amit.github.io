0093b06aa6e0c722fd47c293665c01470afc770d
Amit Kasabe <amitkasabeofficial@gmail.com>
2022-03-01 12:45:22 +0530

    initial commit

---
+++ signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/signup.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Amit</title>
+</head>
+<body>
+  <div class="container-fluid bg-light main">
+      <div class="row main-container">
+        <div class="col-lg-6 col-sm-12 col-md-12 col-12 left bg-dark left">
+              <div class="bg-danger d-flex justify-content-center align-items-center container">
+                <div class="inner-left container d-flex ">
+                  <div class="inner-child-left">
+                    <p>amit</p>
+                  </div>
+                  <div class="inner-child-right">
+                    <p>kasabe</p>
+                  </div>
+                </div>
+              </div>
+              <div class="bg-warning container">
+                kasae
+              </div>
+        </div>
+        <div class="col-lg-6 col-sm-12 col-md-12 col-12 right bg-danger">kasabe</div>
+      </div>
+  </div>
+</body>
+</html>
